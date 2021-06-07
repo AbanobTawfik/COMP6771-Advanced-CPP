bcca65c41b794ce4d05a2f93cd31e5e3d7665620
Abanob Tawfik <goonpizza@hotmail.com>
2021-06-07 10:38:11 +1000

    need to find ALL shortest

--- Assignments/ass1/source/word_ladder.cpp
+++ Assignments/ass1/source/word_ladder.cpp
@@ -1,5 +1,5 @@
 #include "comp6771/word_ladder.hpp"
-#include <queue>
+#include <unordered_set>
 #include <chrono>
 #include <iostream>
 #include <unordered_map>
@@ -15,27 +15,30 @@ namespace word_ladder {
                            const std::unordered_map<std::string, std::string> &visited_front,
                            const std::unordered_map<std::string, std::string> &visited_back,
                            std::vector<std::vector<std::string>> &solutions);
+
     void trim_paths_not_shortest(std::vector<std::vector<std::string>> &solutions);
+
     void
-    update_search(const std::string &parent,
-                  const std::unordered_set<std::string> &neighbours,
-                  std::queue<std::string> &to_explore,
+    update_search(std::unordered_set<std::string> &to_explore,
+                  const std::unordered_map<std::string, std::unordered_set<std::string>> &neighbour_map,
                   std::unordered_map<std::string, std::string> &visited,
                   const std::unordered_map<std::string, std::string> &visited_other_direction,
                   std::vector<std::vector<std::string>> &solutions,
-                  const std::string &from, const std::string &to, bool &found, const bool &forward);
+                  const std::string &from, const std::string &to, bool &found, const bool &forward) ;
+
     auto generate_all_1nn(
             std::unordered_set<std::string> lexicon,
             size_t length) -> std::unordered_map<std::string, std::unordered_set<std::string>>;
-    void show_listings(const std::unordered_map<std::string, std::string> &visited_front){
+
+    void show_listings(const std::unordered_map<std::string, std::string> &visited_front) {
         auto printout = std::unordered_map<std::string, std::vector<std::string>>();
-        for(auto node : visited_front){
+        for (auto node : visited_front) {
             printout[node.second].push_back(node.first);
         }
-        for(auto x : printout){
+        for (auto x : printout) {
             std::cout << x.first << " -> ";
             std::cout << "[";
-            for(auto node : x.second) {
+            for (auto node : x.second) {
                 std::cout << node << ", ";
             }
             std::cout << "]" << std::endl;
@@ -52,55 +55,45 @@ namespace word_ladder {
         }
 
         auto const neighbour_map = word_ladder::generate_all_1nn(lexicon, from.size());
-        auto front = std::queue<std::string>();
-        auto back = std::queue<std::string>();
+        auto front = std::unordered_set<std::string>();
+        auto back = std::unordered_set<std::string>();
         // keep a list of parents too while we're at it, poggies
         auto visited_front = std::unordered_map<std::string, std::string>();
         auto visited_back = std::unordered_map<std::string, std::string>();
-        front.push(from);
-        back.push(to);
+        front.insert(from);
+        back.insert(to);
         auto solutions = std::vector<std::vector<std::string>>();
-        auto found_front = false;
-        auto found_back = false;
+        auto found = false;
         while (not front.empty() and not back.empty()) {
-            auto front_word = front.front();
-            front.pop();
-            auto front_neighbours = get_neighbours(front_word, neighbour_map);
-            update_search(front_word, front_neighbours, front, visited_front, visited_back, solutions, from, to, found_front,
-                          true);
-
-            auto back_word = back.front();
-            back.pop();
-            auto back_neighbours = get_neighbours(back_word, neighbour_map);
-            update_search(back_word, back_neighbours, back, visited_back, visited_front, solutions, from, to, found_back,
-                          false);
-
+            update_search(front, neighbour_map, visited_front, visited_back, solutions, from, to, found, true);
+            update_search(back, neighbour_map, visited_back, visited_front, solutions, from, to, found, false);
         }
         // remove all paths that aren't the shortest
         trim_paths_not_shortest(solutions);
+        std::cout << visited_back["foam"] << std::endl;
         std::sort(solutions.begin(), solutions.end());
-        for(auto i : solutions){
-            for(auto node : i){
+        for (auto i : solutions) {
+            for (auto node : i) {
                 std::cout << node << " ";
             }
-            std::cout <<  std::endl;
+            std::cout << std::endl;
         }
         return solutions;
     }
 
-    void trim_paths_not_shortest(std::vector<std::vector<std::string>> &solutions){
-        if(solutions.size() > 0) {
+    void trim_paths_not_shortest(std::vector<std::vector<std::string>> &solutions) {
+        if (solutions.size() > 0) {
             auto min_length = solutions.begin()->size();
             // find shortest length
-            for(auto solution : solutions){
-                if(solution.size() < min_length){
+            for (auto solution : solutions) {
+                if (solution.size() < min_length) {
                     min_length = solution.size();
                 }
             }
             auto new_solutions = std::vector<std::vector<std::string>>();
             // trim all solutions not of that length
-            for(auto solution : solutions){
-                if(solution.size() == min_length){
+            for (auto solution : solutions) {
+                if (solution.size() == min_length) {
                     new_solutions.push_back(solution);
                 }
             }
@@ -112,6 +105,7 @@ namespace word_ladder {
 
 
     }
+
     auto get_neighbours(const std::string &word,
                         const std::unordered_map<std::string, std::unordered_set<std::string>> &neighbour_map) -> std::unordered_set<std::string> {
         auto neighbours = std::unordered_set<std::string>();
@@ -128,36 +122,39 @@ namespace word_ladder {
     }
 
 
-
     void
-    update_search(const std::string &parent,
-                  const std::unordered_set<std::string> &neighbours,
-                  std::queue<std::string> &to_explore,
+    update_search(std::unordered_set<std::string> &to_explore,
+                  const std::unordered_map<std::string, std::unordered_set<std::string>> &neighbour_map,
                   std::unordered_map<std::string, std::string> &visited,
                   const std::unordered_map<std::string, std::string> &visited_other_direction,
                   std::vector<std::vector<std::string>> &solutions,
                   const std::string &from, const std::string &to, bool &found, const bool &forward) {
-        for (const auto &neighbour : neighbours) {
-            if (visited.count(neighbour)) {
-                continue;
-            }
-            if (visited_other_direction.count(neighbour) ||
-                ((neighbour == from && not forward) || (neighbour == to && forward))) {
-                visited[neighbour] = parent;
-                if (forward) {
-                    generate_solution(neighbour, from, to, visited, visited_other_direction, solutions);
-                } else {
-                    generate_solution(neighbour, from, to, visited_other_direction, visited, solutions);
+        auto new_layer = std::unordered_set<std::string>();
+        for (auto word : to_explore) {
+            auto neighbours = get_neighbours(word, neighbour_map);
+            for (const auto &neighbour : neighbours) {
+                if (visited.count(neighbour)) {
+                    continue;
                 }
-                found = true;
-            }
-            if (!found) {
-                // if we go forward, we update our parent so that the neighbour is a CHILD from the origin word
-                // this way visited keeps track of the parent node from shortest path its found
-                visited[neighbour] = parent;
-                to_explore.push(neighbour);
+                if (visited_other_direction.count(neighbour) ||
+                    ((neighbour == from && not forward) || (neighbour == to && forward))) {
+                    visited[neighbour] = word;
+                    if (forward) {
+                        generate_solution(neighbour, from, to, visited, visited_other_direction, solutions);
+                    } else {
+                        generate_solution(neighbour, from, to, visited_other_direction, visited, solutions);
+                    }
+                    found = true;
+                }
+                if (!found) {
+                    // if we go forward, we update our parent so that the neighbour is a CHILD from the origin word
+                    // this way visited keeps track of the parent node from shortest path its found
+                    visited[neighbour] = word;
+                    new_layer.insert(neighbour);
+                }
             }
         }
+        to_explore = new_layer;
     }
 
     void generate_solution(const std::string &neighbour, const std::string &from, const std::string &to,
@@ -176,7 +173,7 @@ namespace word_ladder {
         auto backward_path = std::vector<std::string>();
         // backtrack from neighbour -> beginning word for the forward path
         // note we storing in reverse order, so we need to reverse this list
-        while(word != from){
+        while (word != from) {
             forward_path.push_back(word);
             // get parent of this node and add it to the list
             word = visited_front.at(word);
@@ -186,7 +183,7 @@ namespace word_ladder {
         forward_path.erase(std::remove(forward_path.begin(), forward_path.end(), neighbour), forward_path.end());
 
         word = neighbour;
-        while(word != to){
+        while (word != to) {
 
             backward_path.push_back(word);
             word = visited_back.at(word);
