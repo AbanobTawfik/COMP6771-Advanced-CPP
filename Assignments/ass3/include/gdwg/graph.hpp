fabad086cd14bedb43801b27c30047a0073c99c7
Abanob Tawfik <goonpizza@hotmail.com>
2021-07-23 15:24:27 +1000

    big changes happening

--- Assignments/ass3/include/gdwg/graph.hpp
+++ Assignments/ass3/include/gdwg/graph.hpp
@@ -34,56 +34,114 @@ namespace gdwg {
         // Your member functions go here
         auto insert_node(N const &value) -> bool;
 
-        auto insert_edge(N const &src, N const &dst, E const &weight) -> bool;
+        auto insert_edge(N const &from, N const &to, E const &weight) -> bool;
 
-        auto replace_node(N const& old_data, N const& new_data) -> bool;
+        auto replace_node(N const &old_data, N const &new_data) -> bool;
 
+        auto merge_replace_node(N const &old_data, N const &new_data) -> void;
 
     private:
+        struct Node;
+        struct Edge;
+        struct set_comparator;
+        struct Raw_Edge;
+
         struct Edge {
-            Edge(const N &node, const E &w = E()) {
-                connected = std::make_shared<N>(node);
-                weight = w;
+            Edge() = default;
+
+            Edge(std::weak_ptr<Node> from, std::weak_ptr<Node> to, E weight) : from{from}, to{to},
+                                                                               weight{weight} {}
+
+            ~Edge() {
+                from.reset();
+                to.reset();
             }
 
-            Edge(const std::shared_ptr<N> &node, const E &w = E()) {
-                connected = node;
-                weight = w;
-            }
-
-            std::weak_ptr<N> connected;
+            std::weak_ptr<Node> from;
+            std::weak_ptr<Node> to;
             E weight;
+        };
 
-            E get_weight() const {
-                return this->weight;
+        struct Node {
+            Node() = default;
+
+            Node(N value) : value{value} {
+                incoming = std::set<std::weak_ptr<Edge>, set_comparator>();
+                outgoing = std::set<std::weak_ptr<Edge>, set_comparator>();
             }
 
-            N get_node() const {
-                return *this->connected;
+            Node(N value, std::set<std::weak_ptr<Edge>> incoming, std::set<std::weak_ptr<Edge>> outgoing) : value{
+                    value}, incoming{incoming}, outgoing{outgoing} {};
+
+            std::set<std::weak_ptr<Edge>, set_comparator> incoming;
+            std::set<std::weak_ptr<Edge>, set_comparator> outgoing;
+            N value;
+        };
+
+        struct set_comparator {
+            bool operator()(const std::shared_ptr<Node> &lhs, const std::shared_ptr<Node> &rhs) const {
+                return lhs.get()->value < rhs.get()->value;
             }
 
-            bool operator<(const Edge &rhs) const {
-                if(rhs.connected.lock().get() == connected.lock().get()){
-                    return false;
+            bool operator()(const std::weak_ptr<Node> &lhs, const std::weak_ptr<Node> &rhs) const {
+                return lhs.lock().get()->value < rhs.lock().get()->value;
+            }
+
+            bool operator()(const Node &lhs, const Node &rhs) const {
+                return lhs->value < rhs->value;
+            }
+
+            // in order to do this in O(log(N) time we need edges to be sorted properly
+            // sort first by from, to, weight in that order
+            bool operator()(const std::shared_ptr<Edge> &lhs, const std::shared_ptr<Edge> &rhs) const {
+                // first compare by from
+                if (lhs.get()->from.lock().get()->value != rhs.get()->from.lock().get()->value) {
+                    return lhs.get()->from.lock().get()->value < rhs.get()->from.lock().get()->value;
                 }
-                return weight < rhs.weight;
+                // if above check failed, means from nodes are identical so now check to nodes
+                if (lhs.get()->to.lock().get()->value != rhs.get()->to.lock().get()->value) {
+                    return lhs.get()->to.lock().get()->value < rhs.get()->to.lock().get()->value;
+                }
+                // now we have same from and to nodes (same edge location), compare by weight now
+                return lhs.get()->weight < rhs.get()->weight;
+            }
+
+            bool operator()(const std::weak_ptr<Edge> &lhs, const std::weak_ptr<Edge> &rhs) const {
+                if (lhs.lock().get()->from.lock().get()->value != rhs.lock().get()->from.lock().get()->value) {
+                    return lhs.lock().get()->from.lock().get()->value < rhs.lock().get()->from.lock().get()->value;
+                }
+                if (lhs.lock().get()->to.lock().get()->value != rhs.lock().get()->to.lock().get()->value) {
+                    return lhs.lock().get()->to.lock().get()->value < rhs.lock().get()->to.lock().get()->value;
+                }
+                return lhs.lock().get()->weight < rhs.lock().get()->weight;
+            }
+
+            bool operator()(const Edge &lhs, const Edge &rhs) const {
+                if (not lhs.from == rhs.from) {
+                    return lhs.from < rhs.from;
+                }
+                if (not lhs.to == rhs.to) {
+                    return lhs.to < rhs.to;
+                }
+                return lhs.weight < rhs.weight;
             }
         };
 
-        struct compare_nodes {
-            bool operator()(const std::shared_ptr<N> &l, const std::shared_ptr<N> &r) const {
-                return *l.get() < *r.get();
-            }
-        };
-
-        std::map<std::shared_ptr<N>, std::set<Edge>, compare_nodes> graph_;
+        // set keeps order so O(logn) lookups due to binary search built into find (RB trees)
+        // nodes contain a list of all their incoming and outgoing connections, and the set of edges
+        // can be used for book keeping
+        // due to nodes containing a set of weak_ptrs for both incoming/outgoing connections, when a edge
+        // is removed from the all_Edges set, all nodes assosicated will have corresponding connections removed too
+        // only book keeping is required for managing the connections on nodes
+        std::set<std::shared_ptr<Node>, set_comparator> all_nodes_;
+        std::set<std::shared_ptr<Edge>, set_comparator> all_edges_;
     };
 
     template<typename N, typename E>
     graph<N, E>::graph(std::initializer_list<N> il1) {
         std::for_each(il1.begin(), il1.end(),
-                      [&](auto node) {
-                          graph_.emplace(std::make_shared<N>(node), std::set<Edge>());
+                      [&](auto value) {
+                          all_nodes_.insert(std::make_shared<Node>(Node{value}));
                       });
 
     }
@@ -92,66 +150,72 @@ namespace gdwg {
     template<typename InputIt>
     graph<N, E>::graph(InputIt first, InputIt last) : graph() {
         std::for_each(first, last,
-                      [&](auto node) {
-                          graph_.emplace(std::make_shared<N>(node), std::set<Edge>());
+                      [&](auto value) {
+                          all_nodes_.insert(std::make_shared<Node>(Node{value}));
                       });
     }
 
     template<typename N, typename E>
     graph<N, E>::graph(graph &&other) noexcept {
-        graph_ = std::move(other.graph_);
-        other.graph_.clear();
+        all_nodes_ = std::move(other.all_nodes_);
+        all_edges_ = std::move(other.all_edges_);
+        if (other != *this) {
+            other.all_nodes_.clear();
+            other.all_edges_.clear();
+        }
     }
 
     template<typename N, typename E>
-    graph<N, E>::graph(const graph &other) : graph_(other.graph_.begin(), other.graph_.end()) {
-        std::for_each(other.graph_.begin(), other.graph_.end(), [&](auto node) {
-            if (graph_.contains(node.first)) {
-                graph_.at(node.first.get()) = other.graph_.at(node.first.get());
-            }
-        });
+    graph<N, E>::graph(const graph &other) {
+        std::copy(other.all_nodes_.begin(), other.all_nodes_.end(), all_nodes_.begin(), all_nodes_.end());
+        std::copy(other.all_edges_.begin(), other.all_edges_.end(), all_edges_.begin(), all_edges_.end());
     }
 
     template<typename N, typename E>
     auto graph<N, E>::operator=(const graph &other) -> graph & {
-        std::for_each(other.graph_.begin(), other.graph_.end(), [&](auto node) {
-            graph_.emplace(node, std::set<Edge>());
-            if (graph_.contains(node.first)) {
-                graph_.at(node.first) = other.graph_.at(node.first);
-            }
-        });
+        std::copy(other.all_nodes_.begin(), other.all_nodes_.end(), all_nodes_.begin(), all_nodes_.end());
+        std::copy(other.all_edges_.begin(), other.all_edges_.end(), all_edges_.begin(), all_edges_.end());
         return *this;
     }
 
     template<typename N, typename E>
     auto graph<N, E>::operator=(graph &&other) noexcept -> graph & {
-        graph_ = std::move(other.graph_);
-        other.graph_.clear();
+        all_nodes_ = std::move(other.all_nodes_);
+        all_edges_ = std::move(other.all_edges_);
+        if (other != *this) {
+            other.all_nodes_.clear();
+            other.all_edges_.clear();
+        }
         return *this;
     }
 
     template<typename N, typename E>
     auto graph<N, E>::insert_node(const N &value) -> bool {
-        if (graph_.find(std::make_shared<N>(value)) == graph_.end()) {
-            graph_.emplace(std::make_shared<N>(value), std::set<Edge>());
+        auto to_insert = std::make_shared<Node>(value);
+        if (all_nodes_.find(to_insert) == all_nodes_.end()) {
+            all_nodes_.insert(to_insert);
             return true;
         }
         return false;
     }
 
     template<typename N, typename E>
-    auto graph<N, E>::insert_edge(const N &src, const N &dst, const E &weight) -> bool {
-        auto check_node_src = graph_.find(std::make_shared<N>(src));
-        auto check_node_dst = graph_.find(std::make_shared<N>(dst));
-        if (check_node_dst == graph_.end() or check_node_src == graph_.end()) {
+    auto graph<N, E>::insert_edge(const N &from, const N &to, const E &weight) -> bool {
+
+        auto check_node_from = all_nodes_.find(std::make_shared<Node>(from));
+        auto check_node_to = all_nodes_.find(std::make_shared<Node>(to));
+        if (check_node_to == all_nodes_.end() or check_node_from == all_nodes_.end()) {
             throw std::runtime_error(
-                    "Cannot call gdwg::graph<N, E>::insert_edge when either src or dst node does not exist");
+                    "Cannot call gdwg::graph<N, E>::insert_edge when either from or to node does not exist");
         }
         // first we will check if the edge already exists, this will be O(log(n)) due to search time for set
-        auto check_edge = graph_.at(std::make_shared<N>(src)).find(Edge{dst, E()});
-        if(check_edge == graph_.at(std::make_shared<N>(src)).end()){
-            auto node = std::find(graph_.begin(), graph_.end(), [&](auto value){return value.first.get() == src;});
-            //graph_.at(std::make_shared<N>(src)).insert(Edge{node.first, weight});
+
+        auto edge_to_insert = std::make_shared<Edge>(*check_node_from, *check_node_to, weight);
+        if (all_edges_.find(edge_to_insert) == all_edges_.end()) {
+            all_edges_.insert(edge_to_insert);
+            // now to update the nodes incoming and outgoing connections for both from and to, O(log(n)) for each
+            check_node_from->get()->outgoing.insert(edge_to_insert);
+            check_node_to->get()->incoming.insert(edge_to_insert);
             return true;
         }
         return false;
@@ -159,16 +223,88 @@ namespace gdwg {
 
     template<typename N, typename E>
     auto graph<N, E>::replace_node(const N &old_data, const N &new_data) -> bool {
-        if(graph_.find(old_data) == graph_.end()){
+        auto old_node = all_nodes_.find(std::make_shared<Node>(old_data));
+        if (old_node == all_nodes_.end()) {
             throw std::runtime_error("Cannot call gdwg::graph<N, E>::replace_node on a node that doesn't exist");
         }
-        if(graph_.find(new_data) != graph_.end()) {
+        if (all_nodes_.find(std::make_shared<Node>(new_data)) != all_nodes_.end()) {
             return false;
         }
-        graph_.emplace(new_data, std::move(graph_.at(old_data)));
-        graph_.erase(old_data);
+        // creating the new node
+        auto new_node = std::make_shared<Node>(new_data);
+        auto updated_in_edges = std::set<std::weak_ptr<Edge>, set_comparator>();
+        auto updated_out_edges = std::set<std::weak_ptr<Edge>, set_comparator>();
+        for (auto in_edge : old_node->get()->incoming) {
+            auto edge_to_insert = std::make_shared<Edge>(
+                    Edge{in_edge.lock().get()->from, new_node, in_edge.lock().get()->weight});
+            all_edges_.erase(in_edge.lock());
+            updated_in_edges.insert(edge_to_insert);
+        }
+        for (auto out_edge : old_node->get()->outgoing) {
+            auto edge_to_insert = std::make_shared<Edge>(Edge{new_node, out_edge.lock().get()->from,
+                                                              out_edge.lock().get()->weight});
+            all_edges_.erase(out_edge.lock());
+            updated_out_edges.insert(edge_to_insert);
+        }
+        new_node.get()->incoming = updated_in_edges;
+        new_node.get()->outgoing = updated_out_edges;
+        all_nodes_.insert(new_node);
+        all_nodes_.erase(old_node);
+        auto z = all_nodes_.find(std::make_shared<Node>(new_data))->get();
         return true;
     }
+
+    template<typename N, typename E>
+    auto graph<N, E>::merge_replace_node(const N &old_data, const N &new_data) -> void {
+        // we can call replace_node for this since sets naturally do not contain duplicates!
+        auto old_node = all_nodes_.find(std::make_shared<Node>(old_data));
+        auto new_node = all_nodes_.find(std::make_shared<Node>(new_data));
+        if (old_node == all_nodes_.end() || new_node == all_nodes_.end()) {
+            throw std::runtime_error(
+                    "Cannot call gdwg::graph<N, E>::merge_replace_node on old or new data if they don't exist in the graph");
+        }
+        // due to the nature of our sets, duplicates are ignored anyways, since sets are compared by elements itself
+        // so we just need to update all edge connections recursively
+//        auto check_duplicates_incoming = std::set<Raw_Edge, raw_edge_comparator>();
+//        for (auto in_edge : new_node->get()->incoming) {
+//            check_duplicates_incoming.insert(Raw_Edge{in_edge.lock().get()->from.lock().get()->value,
+//                                                      in_edge.lock().get()->to.lock().get()->value,
+//                                                      in_edge.lock().get()->weight});
+//        }
+//        auto check_duplicates_outgoing = std::set<Raw_Edge, raw_edge_comparator>();
+//        for (auto out_edge : new_node->get()->incoming) {
+//            check_duplicates_incoming.insert(Raw_Edge{out_edge.lock().get()->from.lock().get()->value,
+//                                                      out_edge.lock().get()->to.lock().get()->value,
+//                                                      out_edge.lock().get()->weight});
+//        }
+//
+//        for (auto in_edge : old_node->get()->incoming) {
+//            if (check_duplicates_incoming.find(
+//                    Raw_Edge{in_edge.lock().get()->from.lock().get()->value, new_data, in_edge.lock().get()->weight}) !=
+//                check_duplicates_incoming.end()) {
+//                continue;
+//            }
+//            check_duplicates_incoming.insert(
+//                    Raw_Edge{in_edge.lock().get()->from.lock().get()->value, new_data, in_edge.lock().get()->weight});
+//            auto edge_to_insert = std::make_shared<Edge>(
+//                    Edge{in_edge.lock().get()->from, new_node, in_edge.lock().get()->weight});
+//            new_node->
+//            all_edges_.erase(in_edge.lock());
+//            updated_in_edges.insert(edge_to_insert);
+//        }
+//        for (auto out_edge : old_node->get()->outgoing) {
+//            auto edge_to_insert = std::make_shared<Edge>(Edge{new_node, out_edge.lock().get()->from,
+//                                                              out_edge.lock().get()->weight});
+//            all_edges_.erase(out_edge.lock());
+//            updated_out_edges.insert(edge_to_insert);
+//        }
+//        new_node.get()->incoming = updated_in_edges;
+//        new_node.get()->outgoing = updated_out_edges;
+//        all_nodes_.insert(new_node);
+        auto z = all_nodes_.find(std::make_shared<Node>(new_data))->get();
+    }
+
+
 } // namespace gdwg
 #endif // GDWG_GRAPH_HPP
 
--- Assignments/ass3/source/client.cpp
+++ Assignments/ass3/source/client.cpp
@@ -5,26 +5,29 @@
 #include "gdwg/graph.hpp"
 
 auto main() -> int {
-    auto x = std::vector<std::string>{"hello", "hi", "bye"};
-    auto g = gdwg::graph<std::string, int>(x.begin(), x.end());
-    auto b = g.insert_edge("hello", "hi", 5);
-    auto c = g.insert_edge("hello", "hi", 5);
-    std::cout << b << std::endl;
-//    auto g = gdwg::graph<std::string, int>();
-//    g.insert_node("hello");
-//    g.insert_node("how");
-//    g.insert_node("are");
-//    g.insert_node("you?");
-//
-//    g.insert_edge("hello", "how", 5);
-//    g.insert_edge("hello", "are", 8);
-//    g.insert_edge("hello", "are", 2);
-//
-//    g.insert_edge("how", "you?", 1);
-//    g.insert_edge("how", "hello", 4);
-//
-//    g.insert_edge("are", "you?", 3);
+//    auto x = std::vector<std::string>{"hello", "hi", "bye"};
+//    auto g = gdwg::graph<std::string, int>(x.begin(), x.end());
+//    g.insert_node("pooo");
+//    auto b = g.insert_edge("hello", "hi", 5);
+//    auto c = g.insert_edge("hello", "hi", 5);
+//    std::cout << b << std::endl;
+    auto g = gdwg::graph<std::string, int>();
+    g.insert_node("hello");
+    g.insert_node("how");
+    g.insert_node("are");
+    g.insert_node("you?");
 
+    g.insert_edge("hello", "how", 5);
+    g.insert_edge("hello", "how", 5);
+    g.insert_edge("hello", "are", 8);
+    g.insert_edge("hello", "are", 2);
+
+    g.insert_edge("how", "you?", 1);
+    g.insert_edge("how", "hello", 4);
+
+    g.insert_edge("are", "you?", 3);
+
+    g.replace_node("hello", "kekw");
 //	std::cout << g << "\n";
 //
 //	auto g2 = gdwg::graph<std::string, int>(g);
