85c0e2f5fc0c3a887981952f3113993c436fe92a
Abanob Tawfik <goonpizza@hotmail.com>
2021-07-29 23:00:13 +1000

    fixing outgoing connections

---
+++ Assignments/ass3/test/graph/Accessors/is_connected.cpp
@@ -0,0 +1,17 @@
+// We don't own this macro; don't prefix it with `COMP6771_`.
+#define CATCH_CONFIG_MAIN // NOLINT(readability-identifier-naming)
+#include "catch2/catch.hpp"
+
+// This file is designed to act as the program entry point. Everything is defined in catch.hpp, so
+// we don't need to worry about doing anything other than indicating our interest to use it via the
+// CATCH_CONFIG_MAIN macro.
+#include "gdwg/graph.hpp"
+
+// Simplest test case, makes sure that when calling the default constructor we get a euclidean
+// vector like [0]
+
+TEST_CASE("default_constructor_ints") {
+    using graph = gdwg::graph<int, int>;
+    const auto default_graph = graph();
+    REQUIRE(default_graph.empty());
+}
